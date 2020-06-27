213b8f43fd967807765e5f4be8afe027cc7f22f8
mnao305 <naomasa305@gmail.com>
2020-06-28 03:21:17 +0900

    refactor: propsの値を先に宣言しておく

--- src/lib-components/SocialShareButton/index.test.ts
+++ src/lib-components/SocialShareButton/index.test.ts
@@ -6,25 +6,22 @@ const localVue = createLocalVue()
 localVue.use(VueCompositionApi)
 
 describe('SocialShareButton/index.vue - Twitter', () => {
+  const props = {
+    url: 'http://www.example.com',
+    text: 'test',
+    service: 'twitter',
+  }
   it('Display the text passed by Props', () => {
     const wrapper = shallowMount(SocialShareButton, {
       localVue,
-      propsData: {
-        url: 'http://www.example.com',
-        text: 'test',
-        service: 'twitter',
-      },
+      propsData: props,
     })
     expect(wrapper.text()).toBe('Tweet')
   })
   it('The url passed in Props is set', () => {
     const wrapper = shallowMount(SocialShareButton, {
       localVue,
-      propsData: {
-        url: 'http://www.example.com',
-        text: 'test',
-        service: 'twitter',
-      },
+      propsData: props,
     })
     expect(wrapper.attributes().href).toBe('https://twitter.com/intent/tweet?url=http%3A%2F%2Fwww.example.com&text=test')
   })
