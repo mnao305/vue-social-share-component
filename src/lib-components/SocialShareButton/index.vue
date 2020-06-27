a512e0873fba9e7556a46732907b1446fbcc8b00
mnao305 <naomasa305@gmail.com>
2020-06-27 12:21:34 +0900

    feat: twitterシェアボタン

--- src/lib-components/SocialShareButton/index.vue
+++ src/lib-components/SocialShareButton/index.vue
@@ -1,10 +1,15 @@
 <template>
-  <button
-    class="social-share-button"
-    @click="clickEvent"
+  <a
+    class="social-share-button twitter"
+    :href="getUrl()"
+    target="_blank"
   >
-    Share
-  </button>
+    <img
+      class="social-share-icon"
+      src="./icons/twitter.svg"
+    >
+    {{ getText() }}
+  </a>
 </template>
 
 <script lang="ts">
@@ -13,18 +18,57 @@ import { defineComponent } from '@vue/composition-api'
 export default defineComponent({
   name: 'SocialShareButton',
   setup () {
-    const clickEvent = () => {
-      console.log('click')
+    const services = {
+      twitter: {
+        text: 'Tweet',
+        url: 'https://twitter.com/intent/tweet?url={url}&text={title}',
+      },
+      facebook: {},
+      hatena: {},
+      pocket: {},
     }
-    return { clickEvent }
+
+    const formatByArr = (serviceUrl: string, url: string, title?: string) => {
+      let text = serviceUrl.replace(/{url}/, encodeURIComponent(url))
+      if (title) text = text.replace(/{title}/, encodeURIComponent(title))
+      return text
+    }
+
+    const getUrl = () => {
+      return formatByArr(services.twitter.url, 'example.com', 'test')
+    }
+
+    const getText = () => {
+      return services.twitter.text
+    }
+
+    return { getUrl, getText }
   },
 })
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .social-share-button {
   border: none;
-  background-color: lightblue;
-  padding: 10px;
+  padding: 14px 40px;
+  border-radius: 5px;
+  display: inline;
+  position: relative;
+  font-weight: bold;
+  text-decoration: none;
+  color: white;
+  text-align: center;
+
+  .social-share-icon {
+    height: 20px;
+  }
+
+  &.twitter {
+    background-color: #1da1f2;
+  }
+
+  &.twitter:hover {
+    background-color: #1397d8;
+  }
 }
 </style>
